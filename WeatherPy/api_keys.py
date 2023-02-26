20370cea21ccb12cbe69cbe651807fdf787bad21
PeiranC <117196071+PeiranC@users.noreply.github.com>
2023-02-26 14:58:38 -0500

    update new

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6ca82289ac5b4a392b7b2fa1ba423eb2"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "fa23c09a64704d919df76f837dd5c696"
